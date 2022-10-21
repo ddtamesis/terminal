9915471745ef23d29d853c06ad952201371c1b1f
ddtamesis <77988084+ddtamesis@users.noreply.github.com>
2022-10-21 18:01:06 -0400

    Add files via upload

---
+++ src/mockedJson.js
@@ -0,0 +1,12 @@
+// simple rectangular csv file! 
+export var mockSimple = "[[\"name\", \"age\", \"school\"],\n    [\"amy\", \"22\", \"brown\"],\n    [\"bob\", \"18\", \"harvard\"], \n    [\"cheng\", \"21\", \"boston college\"], \n    [\"duke benson\", \"19\", \"boston university\"], \n    [\"esther\", \"19\", \"notre dame\"], \n    [\"grace\", \"21\", \"university of southern california\"]]";
+// this is an example of when you have empty string elements
+export var mockSomeEmptyStrings = "[[\"name\", \"age\", \"school\"],\n    [\"amy\", \"\", \"brown\"],\n    [\"bob\", \"18\", \"harvard\"], \n    [\"cheng\", \"21\", \"boston college\"], \n    [\"duke benson\", \"19\", \"\"], \n    [\"\", \"19\", \"notre dame\"], \n    [\"grace\", \"21\", \"\"]]";
+// empty csv file - has rows and columns, but no entries, only empty strings
+export var mockAllEmptyStrings = "[[\"\", \"\", \"\"],\n    [\"\", \"\", \"\"],\n    [\"\", \"\", \"\"],\n    [\"\", \"\", \"\"],\n    [\"\", \"\", \"\"]]";
+// a small csv with just 1 row with 1 element
+export var mockSmall = "[[7]]";
+// array of empty arrays
+export var mockEmptyRows = "[[],\n    [],\n    [],\n    [],\n    []]";
+// blank
+export var mockNoRows = "[]";
