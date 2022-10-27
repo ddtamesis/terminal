38dcdb2a554d45f62c3bb63d3634be3ef46802df
ddtamesis <denise_danielle_tamesis@brown.edu>
2022-10-26 21:50:02 -0400

    weather handler implemented

---
+++ react-project/src/WeatherHandler.tsx
@@ -0,0 +1,15 @@
+import { REPLFunction } from "./ReplFunctions";
+
+function weatherHandler(args: Array<string>): Promise<string> {
+    const lat: string = args[0];
+    const lon: string = args[1];
+    const promise: Promise<string> = fetch(`http://localhost:3232/weather?lat=${lat}&lon=${lon}`)
+        .then(response => response.json())
+        .then(r => {
+            let output: string = (r.result === "success") ? r.temperature : r.result;
+            return output;
+        })
+    return promise;
+}
+
+export {weatherHandler}
