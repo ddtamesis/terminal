d68e4eae0309e699827a0cb3df3f176781d1f1f4
ddtamesis <denise_danielle_tamesis@brown.edu>
2022-10-28 16:17:30 -0400

    get handler changes

--- react-project/src/handlers/GetHandler.tsx
+++ react-project/src/handlers/GetHandler.tsx
@@ -5,7 +5,6 @@ const URL = `http://localhost:3232/`
 export let getHandler : REPLFunction;
 
 getHandler = function(args: Array<string>): Promise<string>  {
-
     return new Promise((resolve) => {
         if (args.length != 1) {
             resolve("Invalid number of arguments. Format as get <filepath>.")
@@ -36,3 +35,32 @@ getHandler = function(args: Array<string>): Promise<string>  {
     )
 }
 
+
+    /*
+    if (args.length != 1) {
+        return Promise.resolve("Invalid number of arguments. Format as get <filepath>.");
+    } else {
+        const loadCSVURL = `${URL}loadcsv?filepath=${args[0]}`
+        const promise: Promise<string> = fetch(loadCSVURL)
+            .then(response => response.json())
+            .then(r => {
+                if (r.result === "success") {
+                    const getCSVURL = `${URL}getcsv`;
+                    fetch(getCSVURL)
+                    .then(response => response.json())
+                    .then(r => {
+                        const csvContent: string | undefined = JSON.stringify(JSON.parse(JSON.stringify(r.data)))
+                        if (r.result != "success") {
+                            return "Error retrieving csv contents.";
+                        } else if (csvContent == undefined) {
+                            return "Error retrieving csv contents.";
+                        } else {
+                            return csvContent;
+                        }
+                    })
+                } else {
+                    return "Unable to properly load file. Ensure the file path is valid.";
+                }
+            })
+        return promise;
+    } */
