95dd06bba8b3bcdf01a90dfc3677d084cd7ea4b5
ddtamesis <denise_danielle_tamesis@brown.edu>
2022-10-29 16:55:23 -0400

    passing 1 get command

--- react-project/src/REPL.test.tsx
+++ react-project/src/REPL.test.tsx
@@ -5,34 +5,50 @@ import REPL, { TEXT_input_box_accessible_name, TEXT_input_text_accessible_name,
      TEXT_input_output_pair_accessible_name, TEXT_repl_command_history_accessible_name } from './REPL';
 import userEvent from '@testing-library/user-event';
 
+beforeEach(() => {
+    render(<REPL />)
+  })
+
 test('renders command history', () => {
-    render(<REPL/>);
     const commandHistory = screen.getByLabelText(TEXT_repl_command_history_accessible_name);
     expect(commandHistory).toBeInTheDocument();
 }
 )
 
 test('renders command input box', () => {
-    render(<REPL/>);
     const inputBox = screen.getByRole("textbox", {name: TEXT_input_text_accessible_name});
     expect(inputBox).toBeInTheDocument();
 })
 
 test('renders submit button', () => {
-    render(<REPL />);
     const buttonElement = screen.getByRole("button", {name: TEXT_submit_button_accessible_name});
     expect(buttonElement).toBeInTheDocument();
 })
 
-test('submitting 1 successful get command', () => {
-    render(<REPL/>);
+test('submitting 1 successful get command', async () => {
     const inputBox = screen.getByRole("textbox", {name: TEXT_input_text_accessible_name});
     const submitButton = screen.getByRole("button", {name: TEXT_submit_button_accessible_name});
 
     userEvent.type(inputBox, 'get data/testing/test-basic.csv');
     userEvent.click(submitButton);
 
-    const commandHistory = screen.getByLabelText(TEXT_repl_command_history_accessible_name);
-    const inputOutputPair = within(commandHistory).getByRole(/.*/, {name: TEXT_input_output_pair_accessible_name + " " + 1});
+    const commandHistory = await screen.findByLabelText(TEXT_repl_command_history_accessible_name);
+    const inputOutputPair = await within(commandHistory).findByLabelText(TEXT_input_output_pair_accessible_name + " " + 1);
     expect(inputOutputPair).toBeInTheDocument();
+})
+
+test('submitting get followed by stats', async () => {
+    const inputBox = screen.getByRole("textbox", {name: TEXT_input_text_accessible_name});
+    const submitButton = screen.getByRole("button", {name: TEXT_submit_button_accessible_name});
+
+    userEvent.type(inputBox, 'get data/testing/test-basic.csv');
+    userEvent.click(submitButton);
+    userEvent.type(inputBox, 'stats');
+    userEvent.click(submitButton);
+
+    const commandHistory = await screen.findByLabelText(TEXT_repl_command_history_accessible_name);
+    const inputOutputPair1 = await within(commandHistory).findByLabelText(TEXT_input_output_pair_accessible_name + " " + 1);
+    const inputOutputPair2 = await within(commandHistory).findByLabelText(TEXT_input_output_pair_accessible_name + " " + 2);
+    expect(inputOutputPair1).toBeInTheDocument();
+    expect(inputOutputPair2).toBeInTheDocument();
 })
